6dabfea9faa4c7a5110b8995be88029983ed7ff8
Re44e <re44e@outlook.com.br>
2021-01-06 15:17:27 -0300

    Configurações iniciais do servidor.

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+
+import express from 'express';
+import bodyparser from 'body-parser'
+
+import compression from 'express';
+
+
+const app = express();
+
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+
+/* Middleware de compactação Gzip para ortimizar o desempenho das requisições */
+app.use(compression());
+
+
+
+export { app };
