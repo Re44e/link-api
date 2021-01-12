12c5c424bd7f4065fc6ccf731e7cb1dd57be33c2
re44e <jean.pita@soterotech.com.br>
2021-01-12 16:14:28 -0300

    Implementa o Helmet como recurso de segurança.

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,17 @@ import express from 'express'
 import compression from 'express'
 import { routes } from './communication/routes'
 import cors from 'cors'
+import helmet from 'helmet'
 
 const app = express();
 app.use(express.json());
 
 /* Middleware de compactação Gzip para ortimizar o desempenho das requisições */
 app.use(compression());
+
+/* Coleção de funções de middlewares de segurança: Proteção contra vulnerabilidades conhecidas na web */
+app.use(helmet());
+
 app.use(cors());
 app.use(routes);
 
